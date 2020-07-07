608f98537b1699e9a9894727ed9baefdf29a2616
Godwin254 <godwinbee15@gmail.com>
2020-07-07 14:08:00 -0400

    add new file
    import utility packages
    
    define scanner object
    
    declare variables
    
    prompt user for input
    
    compute area
    
    display area

---
+++ basics-on-java/src/ComputeAreaWithInput.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ComputeAreaWithInput {
+    public static void main(String[] args){
+        //define scanner object
+        Scanner input = new Scanner(System.in);
+
+        //declare variables
+        double radius, area;
+
+        //prompt user for input
+        System.out.print("Enter some value: ");
+        radius = input.nextDouble();
+
+        //compute area
+        area = Math.pow(radius,2) * 3.14159;
+
+        //display area
+        System.out.println("The area is  " + area);
+
+    }
+}
