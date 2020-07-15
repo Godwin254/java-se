6f112892accdd4db489871971a641314f044bdb6
Godwin254 <godwinbee15@gmail.com>
2020-07-15 11:09:44 -0400

    program to convert celsius to fahrenheit
    declare class
    
    write main method
    
    create scanner object
    
    prompt user for input
    
    compute temperature change
    
    display value of temperature in fahrenheit

---
+++ basics-on-java/src/ChangeToFahrenheit.java
@@ -0,0 +1,22 @@
+//program to convert celsius value to farenheits
+
+import java.util.*;
+
+public class ChangeToFahrenheit {
+    //main method
+    public static void main(String[] args){
+        //create scanner object
+        Scanner input = new Scanner(System.in);
+
+        //prompt user for input in celsius
+        System.out.print("Enter temperature value in Celsius eg. 50.34 : ");
+        double celsius_value = input.nextDouble();
+
+        //compute temperature in fahrenheit
+        double fahrenheit = (9 /5) * celsius_value + 32;
+
+        //display the value of celsius in fahrenheit
+        System.out.println(celsius_value + "Celsius is equivalent to " + fahrenheit + "fahrenheit");
+
+    }
+}
