331373d57438702f06f4d9aeb2bf695463b816c9
Godwin254 <godwinbee15@gmail.com>
2020-07-15 20:06:20 -0400

    program for random subtraction
    import java utilities
    
    create scanner object
    
    generate two random numbers
    
    swap the numbers
    
    prompt user to enter an answer to the subtraction
    
    alert user of the correct answer

---
+++ basics-on-java/src/SubtractionQuiz.java
@@ -0,0 +1,36 @@
+//program to compute random subtraction
+import java.util.*;
+
+public class SubtractionQuiz {
+    public static void main(String[] args){
+        //create scanner object
+        Scanner input = new Scanner(System.in);
+
+        //generate two random numbers
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
+
+        //swap numbers
+        if (number1 < number2){
+
+            int temporaryNumber = number1;
+            number2 = number1;
+            number1 = temporaryNumber;
+
+        }
+
+        //prompt user to perform subtraction
+        System.out.print("What is " + number1 + " - " + number2 + "? ");
+        int answer = input.nextInt();
+
+        //alert user of the correct answer
+        if (number1 - number2 == answer){
+            System.out.println("yey! Correct answer");
+        }
+        else{
+            System.out.println("Oops! Incorrect answer");
+            System.out.println( number1 + " - " + number2 + " = " + (number1 - number2));
+        }
+
+    }
+}
