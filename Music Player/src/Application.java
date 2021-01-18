f95c4970ddcacdfac2778cd1eaec224dd055d218
Godwin254 <godwinbee15@gmail.com>
2021-01-18 12:55:20 +0300

    New Commits

---
+++ Music Player/src/Application.java
@@ -0,0 +1,15 @@
+
+//Recently added music
+//Favorite music
+//Albums
+//Artist
+//Folders
+//Tackles
+
+
+public class Application {
+
+    public static void main(String[] args){
+        System.out.println("Music App");
+    }
+}
