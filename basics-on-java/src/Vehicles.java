69a1799e9f819e9032fa514d8615571dcd32ee10
Godwin254 <godwinbee15@gmail.com>
2021-02-02 23:49:17 +0300

    add switch cases

---
+++ basics-on-java/src/Vehicles.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Vehicles {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //prompt input of an animals name
+        System.out.println("Enter an animals name");
+        String animalName = sc.next();
+
+        String sound = sounds(animalName);
+
+        System.out.println(sound);
+
+    }
+
+    static String sounds(String animal){
+
+        switch (animal){
+            case "cow":
+                System.out.printf("Moo Moo%n");
+                break;
+            case "dog":
+                System.out.printf("Goo Goo%n");
+                break;
+            case "bee":
+                System.out.printf("hiiiiiiizzzzzzz%n");
+                break;
+            case "cat":
+                System.out.printf("Miaaaaauuuuu%n");
+                break;
+            default:
+                System.out.printf("That is not an animal%n");
+        }
+
+        return animal;
+    }
+
+}
